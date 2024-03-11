663a903f9cf54f26189fc8a8eac7096b61250ea7
Omphile Morwane <omphilemorwane@gmail.com>
2024-03-11 21:35:25 +0200

    Defining javascript in HTML

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ function validateSyntax() {
   // Validation logic goes here
   let result = ""; // Placeholder for validation result
 
-  if (RegExp.test(input)) {
+  let regex = /^pet_[a-zA-Z0-9]+$/;
+
+  if (regex.test(input)) {
     result = "Valid Syntax";
   } else {
     result = "Invalid Syntax";
