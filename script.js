6da5593942d00c599ffda3ca5c9f0ecc8d7ede15
Omphile Morwane <omphilemorwane@gmail.com>
2024-03-12 13:57:34 +0200

    Changed the innertext to innerHTML

--- script.js
+++ script.js
@@ -6,15 +6,11 @@ function validateSyntax() {
   let regex = /^pet_[a-zA-Z0-9]+$/;
 
   if (regex.test(input)) {
-    result = "Valid Syntax";
-    document.getElementById("redDot").style.visibility = "hidden";
-    document.getElementById("greenDot").style.visibility = "visible";
+    result = 'Valid Syntax <span class="greenDot"></span>';
   } else {
-    result = "Invalid Syntax";
-    document.getElementById("redDot").style.visibility = "visible";
-    document.getElementById("greenDot").style.visibility = "hidden";
+    result = 'Invalid Syntax <span class="redDot"></span>';
   }
   // Check if input starts with 'pet_' and followed by alphanumeric characters
 
-  document.getElementById("result").innerText = result;
+  document.getElementById("result").innerHTML = result;
 }
